3364f6990954a4baa4e8d4ef1beaf69fd8bbeff3
Mateo Balcorta <misterrattail@gmail.com>
2018-05-14 14:14:57 -0700

    Convert to class Component.

--- js/containers/Footer.jsx
+++ js/containers/Footer.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Logo from './Logo.jsx';
+import logoObj from '../logoObj'
 
-function Footer(){
+
+class Footer extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            logos: null
+        }
+    }
+
+    componentDidMount(){
+       let logos = logoObj.map(({ src, alt}) => 
+            <Logo key={src} alt={alt} src={src}/>
+        )
+        this.setState({logos})
+    }
+    
+    render(){
+       
+
         return (
             <div id="footer">
                 <div className="footer">
@@ -10,111 +31,7 @@ function Footer(){
                             <p>In partnership with: </p> <img src="images/DailyKosLogo.png" />
                         </div>
                         <div className="logos" style={{display: "flex", flexFlow: "row wrap", justifyContent: "center", alignItems: "center"}}>
-                        
-                            <a target="_blank">
-                                <img alt="18 Million Rising" src="images/18mr_logo_short.png" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="American Family Voices (AFV)" src="images/PasAFV.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Center for Media Justice" src="images/CMJ logo hi-res.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Center for Popular Democracy Action" src="images/cpd-action-logo.png" />
-                            </a>
-                            
-                             <a target="_blank">
-                                <img alt="Climate Hawks Vote" src="images/CHV Logo2.jpg" />
-                            </a>
-                                     
-                            <a target="_blank">
-                                <img alt="Color of Change" src="images/COC-Full-Color.png" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Common Cause" src="images/CCLogo_Stacked_NoTagline.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Common Dreams" src="images/CD_stacked_white.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Consumer Reports" src="images/consumer-reports.png" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Corporate Accountability" src="images/CA_LogoTag_Large.png" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Courage Campaign" src="images/Courage-Logo-Color-High-Rez.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Democracy for America" src="images/DFA-Logo-bottom-white-400.png" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Friend of the Earth" src="images/FOE_logo_color.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Just Foreign Policy" src="images/just-foreign-policy.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Open Media" src="images/open-media.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="People Demanding Action" src="images/Logo-PeopleDemandingAction.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="People For the American Way" src="images/PFAW.png" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Presente Action" src="images/PresenteAction2016.png" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Progress America" src="images/progress-america-logo-1.png" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Progressive Congress Action Fund" src="images/IMG_8465.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="RootsAction.org" src="images/RootsAction.png" />
-                            </a>  
-
-                            <a target="_blank">
-                                <img alt="The Nation" src="images/Nation.jpg" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="The Zero Hour" src="images/TZH Logo.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Watchdog" src="images/LOGO_Watchdog-v04.png" />
-                            </a>
-
-                            <a target="_blank">
-                                <img alt="Win Without War" src="images/winwithoutwar.jpg" />
-                            </a>
-                            
-                            <a target="_blank">
-                                <img alt="Workign Families Party" src="images/wfp.jpg" />
-                            </a>
-                           
+                            {this.state.logos}
                         </div>
                         <div>
                             <div className="press-inquiries">
@@ -129,7 +46,7 @@ function Footer(){
                                 </p>
                             </div>
                             <div className="social-media">
-                                <a className="twitter" href="https://twitter.com/intent/tweet?text=This%20is%20a%20%23RedAlert%21%20The%20Senate%20will%20vote%20soon%20on%20%23NetNeutrality.%20Contact%20Congress%20TODAY%20and%20tell%20them%20they%20must%20vote%20%E2%80%98YES%E2%80%99%20on%20the%20CRA%20resolution%20to%20save%20the%20open%20internet%20https%3A%2F%2Fstopthefcc.net%2F%20" target="_blank">
+                                <a className="twitter" href="https://twitter.com/intent/tweet?text=Call%20Congress%21%20Tell%20them%20they%20must%20vote%20%E2%80%98yes%E2%80%99%20on%20the%20CRA%20resolution%20to%20save%20the%20open%20internet%20and%20restore%20%23NetNeutrality%20https%3A%2F%2Fstopthefcc.net%2F" target="_blank">
                                     <img src="images/twitter_white.svg" />
                                     <span>Share on twitter</span>
                                 </a>
@@ -142,6 +59,7 @@ function Footer(){
                     </div>
                 </div>
             </div>);
+    }
 }
 
 export default Footer;
