5c8f33bd4d110fcb933a679bf80f3e79e0bedf8e
Mateo Balcorta <misterrattail@gmail.com>
2018-05-14 14:10:07 -0700

    Add text object.

---
+++ js/textObj.js
@@ -0,0 +1,21 @@
+export const allText =  {
+    headerText: {
+        headerText: "Red Alert:",
+        subHeader: "Tell the Senate to Restore Net Neutrality!"
+    },
+    subHeaderText :{
+        headerText: "The FCC voted to let Big Cable ruin the internet. But on Wednesday, May 16th, the Senate will vote on a resolution to overturn the FCC and restore net neutrality",
+        subHeader : "As of now, 50 senators have said they will vote ‘yes,’ and we need just 51 to guarantee victory.",
+        subSubHeader : "It’s go time! Contact Congress today."
+    },
+    disclaimer: "One or more of the participating organizations (listed at bottom) may email you about their campaigns.",
+    modalText: {
+        h1: "Thanks for signing.",
+        p1: "Please call Congress and tell your lawmakers to overturn the FCC and restore net neutrality:  ",
+        p21: "We’ll connect you to your lawmakers.  You can use this script — just introduce yourself, be polite, and say:",
+        p22: '"I support Title Two net neutrality and I urge you to vote for the ‘resolution of disapproval’ to restore net neutrality."',
+        p3: "Attend an event on Monday:",
+        p4: "On May 14th, activists across the country will be gathering outside of the offices of key lawmakers.  Please sign up for an event near you here (or consider hosting one if there isn't one near you yet):"
+    }
+   
+}
